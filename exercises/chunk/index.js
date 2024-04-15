1b15d9d6e1faac41d312b0ebddc3dbcd9a5d953c
justin0979 <justinm0979@gmail.com>
2024-04-15 13:38:46 -0500

    add js solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunks = [];
+
+  while (array.length > size) {
+    chunks.push(array.splice(0, size));
+  }
+  chunks.push(array);
+
+  return chunks;
+}
 
 module.exports = chunk;

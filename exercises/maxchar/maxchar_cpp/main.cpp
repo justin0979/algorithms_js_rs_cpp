fb753ae920e515526c4cbefbb13c25e6e0ed13de
justin0979 <justinm0979@gmail.com>
2024-04-21 16:05:07 -0500

    add 4 more maxchar cases

--- exercises/maxchar/maxchar_cpp/main.cpp
+++ exercises/maxchar/maxchar_cpp/main.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 #include "maxchar.h"
 
+void print_maxchar_run(std::string input, char solution);
+
 int main() {
-  std::string input = "bzcdefghijklmnzzzzz";
-  char solution = 'z';
-  if(solution == maxchar(input)) {
-    std::cout << "The max char in \"" << input << "\" is " << solution << std::endl;
-  } else {
-    std::cout << "Your program is busted." << std::endl;
-  }
+  print_maxchar_run("bzcdefghijklmnzzzzz", 'z');
+  print_maxchar_run("abcdefghijklmnaaaaa", 'a');
+  print_maxchar_run("ab1c1d1e1f1g1", '1');
+  print_maxchar_run("cqqyyyabcabcddd123333zyyrrzyyqaayyyyyyyyyayz9", 'y');
   return 0;
+}
+
+void print_maxchar_run(std::string input, char solution) {
+  if(solution == maxchar(input)) {
+    std::cout << "The max char in \"" << input << "\" is " << "'" <<
+      solution << "'." <<  std::endl;
+  } else {
+    std::cout << "Your program is busted, it gave the wrong max character." << std::endl;
+  }
 }
